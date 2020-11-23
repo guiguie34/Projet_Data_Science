e01a789a8da53dc87a2b72bdd04af022be3b2e00
Guillaume <guillaume.chebib@hotmail.fr>
2020-11-23 19:48:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+
+import pandas
+import operator
+import json
+
+
+def modification_texte(message):
+    ponctuation = [",", ";", ":", ".", "?", "!", "«", "»", "(", ")", "\"", "…", "'", "-", "’"]
+    message2 = ""
+    for c in message:
+        if (c in ponctuation) or (c == "\n"):
+            message2 += ""
+        else:
+            message2 += c
+    return message2
+
+
+def nb_occ(message, occurrences):
+
+    message = message.split(' ')
+    for c in message:
+        occurrences[c] = occurrences.get(c, 0) + 1
+
+    return occurrences
+
+
+def get_words_content(df):
+    index = df.index
+    number_of_rows = len(index)
+    occurrences = {}
+    for i in range(0, number_of_rows):
+        courant = df['content'][i]
+        courant = modification_texte(courant)
+        occurrences = nb_occ(courant, occurrences)
+        print(i)
+    occurrences_sorted = sorted(occurrences.items(), key=operator.itemgetter(1))
+    print(occurrences_sorted)
+
+    a_file = open("data.json", "w")
+    json.dump(occurrences_sorted, a_file)
+    a_file.close()
+
+
+if __name__ == '__main__':
+    data = pandas.read_csv("data2.csv", sep=',', low_memory=False)
+    data.fillna(" ", inplace=True)  # Replace the null value by a space
+    df = pandas.DataFrame(data);
+    # get_words_content(df)
