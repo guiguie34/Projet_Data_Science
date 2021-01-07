1c79dd1f528c2d1973c1abcdc1408b7b4ed5877d
Guillaume <guillaume.chebib@hotmail.fr>
2021-01-07 16:38:23 +0100

    calcul p-value + boxplot

--- Code/graph/anova_example.py
+++ Code/graph/anova_example.py
@@ -18,21 +18,22 @@ with open("Generated Data/dataForAnova5.json") as f:
 #
 # # generate a boxplot to see the data distribution by treatments. Using boxplot, we can
 # # easily detect the differences between different treatments
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 # #
-# ax = sns.boxplot(x='variable', y='value', data=df, color='#99c2a2')
-# ax = sns.swarmplot(x="variable", y="value", data=df, color='#7d0013')
-# plt.show()
+
 # # # load package
 import scipy.stats as stats
 #
 # df.dropna(axis = 0, how = 'any')
 # print(df)
 df = df[df['time'].notna()]
+df.to_csv("Generated Data/dataForAnova6.csv",header=True,index=True)
 df.info()
 #df[1] = df[1].astype('float', errors='ignore')
-
+ax = sns.boxplot(x='theme', y='time', data=df, color='#99c2a2')
+#ax = sns.swarmplot(x="theme", y="time", data=df, color='#7d0013')
+plt.show()
 #print(df)
 # for i in df.index:
 #     if np.isnan(df["time"][i]):
@@ -60,7 +61,22 @@ model = ols('time ~ C(theme)', data=df).fit()
 anova_table = sm.stats.anova_lm(model, typ=2)
 print(anova_table)
 
-
+# from scipy.stats import bartlett
+# #print(df.groupby('theme').count())
+# tuk = {}
+# for index, row in df.iterrows():
+#     if row["theme"] not in tuk:
+#         tuk[row["theme"]]= list()
+#     tuk[row["theme"]].append(row["time"])
+#
+# tuk2=[]
+# for k,v in tuk.items():
+#     tuk2.append(v)
+#
+# #print(tuk2)
+# print([x for x in tuk2])
+# stat, p = bartlett(tuk2[0],tuk2[1])
+# print(p)
 # # ANOVA table using bioinfokit v1.0.3 or later (it uses wrapper script for anova_lm)
 # from bioinfokit.analys import stat
 #
