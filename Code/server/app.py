e558d7f5a339924727f70a449a3761382fb4642b
LaunoisRemy <remy.launois@gmail.com>
2021-01-09 10:43:52 +0100

    Avancement site web

--- Code/server/app.py
+++ Code/server/app.py
@@ -3,18 +3,13 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import dash_table
-import plotly.express as px
 from Code.graph import anova
 from Code.utils_mail import utils
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
 
 import pandas as pd
 
